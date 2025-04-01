e6c49ee8f5474e9006bfc99959fe44edba779435
Asit Behera <cba.ab18@gmail.com>
2025-04-01 17:03:39 +0530

    modifed some boilerplate code and added comments.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,15 @@
-import { createServer } from "./server";
+/**
+ * * Using server.listen() gives more flexibility over the Express app.listen()
+ * * 1. Ability to test the express app as some testing library just need an instance of express app.
+ * * 2. Ability to extend features like implementing websockets
+ * * 3. We can switch between http and https servers pretty easily.
+ */
+
+import { createServer } from "node:http";
+import app from "./server";
 
 const port = process.env.PORT || 3001;
-const server = createServer();
+const server = createServer(app);
 
 server.listen(port, () => {
   console.log(`api running on ${port}`);
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,25 +1,37 @@
+/**
+ * * This file is used to configure the express app
+ * * Basically we can instanciate Express app and set app level middlewares in this file
+ */
 import { json, urlencoded } from "body-parser";
 import express, { type Express } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import path from "node:path";
 
-export const createServer = (): Express => {
-  const app = express();
-  app
-    .disable("x-powered-by")
-    .use(morgan("dev"))
-    .use(urlencoded({ extended: true }))
-    .use(json())
-    .use(cors());
+const app = express();
 
-  console.log(" PATH :: ", path.resolve(__dirname, "web"));
+app
+  .disable("x-powered-by")
+  .use(morgan("dev"))
+  .use(urlencoded({ extended: true }))
+  .use(json())
+  .use(cors());
 
-  app.use("/", express.static(path.resolve(__dirname, "web")));
+/**
+ * * API routes.
+ */
+app.get("/api/:name", (req, res) => {
+  return res.json({ message: `hello ${req.params.name}` });
+});
 
-  app.get("/api/:name", (req, res) => {
-    return res.json({ message: `hello ${req.params.name}` });
-  });
+/**
+ * ! This is not the ideal way to host static files.
+ * * I understand that these files should be served behind a CDN or a load balancer.
+ * * But for this project, it makes sense.
+ *
+ * * I kept static serving at the end because if we hit a bad endpoint,
+ * * the 404 error will simply fall back to React Router.
+ */
+app.use("/", express.static(path.resolve(__dirname, "web")));
 
-  return app;
-};
+export default app;
