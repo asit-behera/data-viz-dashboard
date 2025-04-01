37fb7070afe4b0f013ef6a34f0fef2cdc4b1c7e9
Asit Behera <cba.ab18@gmail.com>
2025-04-01 15:57:40 +0530

    fixes for deployment

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,6 +2,7 @@ import { json, urlencoded } from "body-parser";
 import express, { type Express } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "node:path";
 
 export const createServer = (): Express => {
   const app = express();
@@ -10,13 +11,15 @@ export const createServer = (): Express => {
     .use(morgan("dev"))
     .use(urlencoded({ extended: true }))
     .use(json())
-    .use(cors())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
-    })
-    .get("/status", (_, res) => {
-      return res.json({ ok: true });
-    });
+    .use(cors());
+
+  console.log(" PATH :: ", path.resolve(__dirname, "web"));
+
+  app.use("/", express.static(path.resolve(__dirname, "web")));
+
+  app.get("/api/:name", (req, res) => {
+    return res.json({ message: `hello ${req.params.name}` });
+  });
 
   return app;
 };
